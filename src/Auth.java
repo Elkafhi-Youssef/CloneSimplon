b97a3ebff30f5903b837fe35ab587b867edcef6c
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-10-02 16:46:30 +0100

    add auth functions admin/formateur

--- src/Auth.java
+++ src/Auth.java
@@ -1,11 +1,46 @@
+import global.GloabState;
+import module.ConnectDb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
 public class Auth {
 
     public String email;
     public String password;
+    static Connection con = ConnectDb.con;
 
 
-    public boolean login(String email,String password){
 
-        return true;
+    public static boolean login(String email,String password){
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement("select * from admin where email = ? and password = ?");
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.next();
+        }catch (SQLException e) {
+            System.out.println("Error in Login Function");
+            throw new RuntimeException(e);
+        }
+    }
+    public static int loginFormateur(String email,String password){
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement("select * from formateurs where email = ? and password = ?");
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id_formateur");
+                GloabState.idLoginUser = id;
+
+            }
+            return GloabState.idLoginUser;
+        }catch (SQLException e) {
+            System.out.println("Error in Login Function");
+            throw new RuntimeException(e);
+        }
     }
 }
