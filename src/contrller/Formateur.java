192f2bdd64243c8771211c71973beea219fce037
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-10-02 16:53:08 +0100

    add class formateur

---
+++ src/contrller/Formateur.java
@@ -0,0 +1,14 @@
+package contrller;
+
+import contrller.Personne;
+
+public class Formateur  extends Personne {
+    static int idGloabFormateur = 1;
+    protected int id_formateur;
+
+//    public Formateur(int id_formateur,String Role, String fullNmae, String email, String password){
+//        super(Role,fullNmae,email,password);
+//        this.id_formateur = id_formateur;
+//        idGloabFormateur++;
+//    }
+}
