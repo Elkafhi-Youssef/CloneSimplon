d6e83edc2280eed56f993e285ba39ae537c1112c
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-10-02 16:53:26 +0100

    global state

---
+++ src/global/GloabState.java
@@ -0,0 +1,16 @@
+package global;
+
+import java.util.ArrayList;
+import contrller.Apprenant;
+public class GloabState {
+
+//    static ArrayList<contrller.Apprenant> appsWithoutPromo;
+     public static ArrayList<Apprenant> Apprenants = new ArrayList();
+//    static ArrayList<contrller.Apprenant> Promos;
+//    static ArrayList<contrller.Apprenant> Admin;
+//    public static ArrayList<contrller.Formateur> formateurs = new ArrayList();
+    public static int  idLoginUser;
+    public static int idPromo;
+    public static int idBrief;
+
+}
