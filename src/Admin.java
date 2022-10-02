d12c0e2b655cbbb200e41b3419ee793957b74c77
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-10-02 16:48:13 +0100

    add functionality of admin

--- src/Admin.java
+++ src/Admin.java
@@ -1,46 +1,55 @@
+import contrller.Personne;
+import module.Users;
+
 import java.util.Scanner;
 import java.util.Random;
-public class Admin extends Personne{
+public class Admin extends Personne {
     Random random = new Random();
   static  int id_admin = 1;
 
 
-
-    public Admin(int id_admin,String Role,String fullName,String email, String password){
-        super(Role,fullName,email,password);
-        id_admin++;
-
+    public Admin(){
+        super();
     }
+//    public Admin(int id_admin,String fullName,String email, String password){
+//        super(fullName,email,password);
+//        id_admin++;
+//
+//    }
 
-    public static void creerFormateur(){
+    public  void creerFormateur(){
         Scanner choice = new Scanner(System.in);
-        System.out.println("enter Role of user");
-            String Role = choice.nextLine();
-            System.out.println("enter name");
-            String fullNmae =choice.nextLine();
-//            int id_user = random.nextInt(30);
-
-            System.out.println("enter email");
-            String email =choice.nextLine();
-            System.out.println("enter password");
-            String password =choice.nextLine();
-        Formateur newFormateur = new Formateur(Formateur.idGloabFormateur,Role,fullNmae,email,password);
-        GloabState.formateurs.add(newFormateur);
-
-
-    }
-    public static void creerApprenant(){
-        Scanner choice = new Scanner(System.in);
-        System.out.println("enter Role of user");
-        String Role = choice.nextLine();
         System.out.println("enter name");
-        String fullNmae =choice.nextLine();
+        String fullName =choice.nextLine();
         System.out.println("enter email");
         String email =choice.nextLine();
         System.out.println("enter password");
         String password =choice.nextLine();
-        Apprenant newApprenant = new Apprenant(Apprenant.idGlobalApp,Role,fullNmae,email,password,0);
-        GloabState.Apprenants.add((Apprenant) newApprenant);
+        if(Users.addFormateur(fullName,email,password)){
+            System.out.println("the formateur create successful");
+        }
+    }
+    public  void creerApprenant(){
+        Scanner choice = new Scanner(System.in);
+        System.out.println("enter name");
+        String fullName =choice.nextLine();
+        System.out.println("enter email");
+        String email =choice.nextLine();
+        System.out.println("enter password");
+        String password =choice.nextLine();
+        if(Users.addApprenant(fullName,email)){
+            System.out.println("the apprenant create successful");
+        }
+
+
+    }
+    public  void creerPromo(){
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Enter the promo name");
+        String promoName =choice.nextLine();
+        if(Users.addPromo(promoName)){
+            System.out.println("the new promo create successful");
+        }
 
 
     }
